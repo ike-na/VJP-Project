a78c59aba4c8420637801b8ba77e35232ef68c7e
Iikka Näsälä <111114906+ike-na@users.noreply.github.com>
2023-05-18 20:25:20 +0300

    layoutti uusiks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
+import Home from './pages/home.js'
 import Video from './pages/video.js'
 import About from './pages/about.js'
-import Layout from "./pages/layout.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
+        <header>
+        <div className="container">
+          <nav>
+                <ul>
+                    <li><Link to="/">Koti</Link></li>
+                    <li><Link to="/video">Video</Link></li>
+                    <li><Link to="/about">Lisää</Link></li>
+                </ul>   
+            </nav>
+            </div>
+        </header>
           <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path="app" element={<App />} /> 
+            <Route path="/" element={<Home />} />
             <Route path="video" element={<Video />} />
-            <Route path="app" element={<App />} />
             <Route path="about" element={<About />} />
-          </Route>
           </Routes>
         </BrowserRouter>
     </div>
