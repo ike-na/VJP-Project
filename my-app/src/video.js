9b2e62d8292a6f6271db5000f640f7a955a21d9f
Niko Vänttilä <niko.vanttila@gmail.com>
2023-05-18 12:47:13 +0300

    starting point for video comments

--- my-app/src/video.js
+++ my-app/src/video.js
@@ -1,9 +1,15 @@
+import Comments from "./comments/comments.js";
+
 const Video = () => {
   return (
     <div className="App">
       <video width="750" height="500" controls >
         <source src="video.mp4" type="video/mp4"/>
       </video>
+      <Comments
+        commentsUrl="http://localhost:3004/comments"
+        currentUserId="1"
+      />
     </div>
   );
 };
