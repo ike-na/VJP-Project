8a9425ad5fed5d7fb36f3e41722b0ab9128d5a75
Iikka Näsälä <111114906+ike-na@users.noreply.github.com>
2023-05-18 18:47:00 +0300

    lisää stailausta

--- my-app/src/components/Comment.js
+++ my-app/src/components/Comment.js
@@ -2,7 +2,7 @@ export function CommentFrom(){
     return (
         <form>
             <div
-                className="Comment-form.container">
+                className="container">
                     <textarea className="tekstikenttä"/>
                     <button className="nappi" type="submit">Lähetä</button>
             </div>
